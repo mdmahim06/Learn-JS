61499c73e53c6826a279e423b79a419acadc37e7
MD Mahim All Saklain <129942610+mdmahim06@users.noreply.github.com>
2024-09-17 21:26:36 +0600

    Update 03_conversion_oparation.js
    add some comments

--- 03_conversion_oparation.js
+++ 03_conversion_oparation.js
@@ -3,6 +3,9 @@ let score = "33";
 console.log(typeof score);
 console.log(typeof(score));
 
-let valueInNumber = Number(score);
+let valueInNumber = Number(score); 
+
+// The Number() function converts a valid numeric string to a number.
+// It returns NaN if the string cannot be parsed as a number.
 
 console.log(typeof valueInNumber);
