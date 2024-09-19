25406f3ead2ef1048fef46b8f9899044dc846c03
MD Mahim All Saklain <mdmahimbd2033@gmail.com>
2024-09-19 15:07:06 +0600

    complex operations

---
+++ 04_conversion_oparation.js
@@ -0,0 +1,77 @@
+// How to get a negative value
+
+	// let value = 3;
+	// let negValue = -value;
+
+	// console.log(negValue);
+
+
+// JavaScript basic Math Operations
+
+	// conosole.log(2 + 2);
+	// conosole.log(2 - 2);
+	// conosole.log(2 * 2);
+	// conosole.log(2 ** 2);
+	// conosole.log(2 / 2);
+	// conosole.log(2 % 2);
+
+// How to connect and concatenation between
+
+	// let str1 = "MD Mahim";
+	// let str2 = " All Saklain";
+
+	// let str3 = str1 + str2;
+
+	// console.log(str3);
+
+// let's learn some complex operations with string and number
+
+	// console.log(1 + "2"); // Output is 12
+	// console.log("1" + 2); // Output is 12
+	// console.log("1" + 2 + 2); // Output is 122
+	// console.log(1 + 2 + "2"); // Output is 32
+
+	// It's mean when a number is assign in first so 
+
+	// console.log(typeof("1" + 2 + 2)); // Output is string
+
+	// console.log(true); // Output true as a boolean value
+	// console.log(+true); // Output is 1
+	// console.log(typeof(+true)); // Output is number	
+
+	// console.log(+""); // Output is 0
+
+	// let num1, num2, num3;
+
+	// num1 = num2 = num3 = 2 + 2;
+
+	// It's assign the value of 2 + 2 sum in all variables (num1, num2, num3)
+
+	// console.log(num1, num2, num3); // output is 4 4 4
+
+// prefix and postfix test
+
+	// prefix
+
+		let x,y;
+		x = 3;
+
+		y = x++;
+
+		// here the increment operator increments and return the value before incrementing
+
+		console.log(`x : ${x}, y : ${y}`);
+
+	// postfix
+
+		let x,y;
+		x = 3;
+
+		y = ++x;
+
+		// here the increment operator increments and return the value after incrementing
+
+		console.log(`x : ${x}, y : ${y}`);
+
+		
+	
