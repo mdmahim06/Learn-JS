12446062f611a28e0ea966201ec21513315ec25e
MD Mahim All Saklain <mdmahimbd2033@gmail.com>
2024-09-19 16:58:24 +0600

    complete

--- 05_comparison.js
+++ 05_comparison.js
@@ -1,1 +1,30 @@
 // Comparison
+
+	// console.log(2 > 1); // check greater then
+	// console.log(2 >= 1); // check greater then and equal
+	// console.log(2 < 1); // check less then
+	// console.log(2 <= 1); // check less then and equal
+	// console.log(2 == 1); // check equal
+	// console.log(2 === 1); // check equal with types
+	// console.log(2 != 1); // check not equal
+	// console.log(2 !== 1); // check not equal with types
+
+	// all are return a boolean value
+
+	// console.log("2" > 1); // output is true
+	// console.log("02" > 1); // output is true
+
+	// console.log(null > 0); // output false
+	// console.log(null == 0); // output false
+	// console.log(null >= 0); // output true
+	// console.log(null <= 0); // output true
+
+	// comparisons covert null to a number, treating it as 0.
+
+	// console.log(undefined > 0); // output false
+	// console.log(undefined == 0); // output false
+	// console.log(undefined >= 0); // output false
+	// console.log(undefined <= 0); // output false
+
+	// comparisons covert undefined to a number, treating it as NaN.
+
